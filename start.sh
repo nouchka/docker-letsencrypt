5d68a1164e8e05537867c95e0411fefe05c1a692
nouchka <github@katagena.com>
2018-05-30 13:35:17 +0000

    create directory

--- start.sh
+++ start.sh
@@ -4,6 +4,7 @@
 mkdir -p /certs/.well-known/
 echo 'GGT' > /certs/.well-known/index.html
 
+mkdir -p /etc/letsencrypt/live/
 
 CRT_LIST=/etc/letsencrypt/live/crt-list.txt.tmp
 FLAG=/certs/.well-known/loading.html
